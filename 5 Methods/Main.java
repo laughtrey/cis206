4d81e68b4010947421138bf3ec23e33463bd1984
laughtrey <laughtrey@gmail.com>
2019-03-18 06:46:53 -0700

    Submitted project. Not sloppy.

--- 5 Methods/Main.java
+++ 5 Methods/Main.java
@@ -13,27 +13,37 @@ public class Main
 public static void main(String[] args)
 {
 	String str = "abc";
-	String str2 = "California";
+	String str2 = "that";
 	String str3 = "Supercalifragilisticexpialidocious";
+	String str4 = "California";
+	String str5 = "March";
+	String str6 = "Java";
 
 	System.out.println(scramble(str));
 	System.out.println(scramble(str2));
 	System.out.println(scramble(str3));
+	System.out.println(scramble(str4));
+	System.out.println(scramble(str5));
+	System.out.println(scramble(str6));
 }
 public static String scramble(String word)
 {
 	String scrambled = word;
 
 	if (word.length() > 3) {
-		int e = word.length() - 1;                                      // assigns the second to last character an integer value
-		String to_scramble = word.substring(1, e);                      // so that here I can scramble everything EXCEPT first and last char
-		int i = (int)(Math.random() * to_scramble.length());            // this picks a random character EXCEPT first and last
-		if (i == 0) {i += 1;}
-		int j = i + 1;                                                  // this gets the one right after it
-		String Beginning = word.substring(0, i);                        // creates a string before the randomly picked character
-		String End = word.substring(j + 1);                             // creates a string after the second picked character to the end of the word
-		scrambled = Beginning + word.charAt(j) + word.charAt(i) + End;  // concatenates the word with scrambled letters inside
+		int i = (int)(Math.random() * (word.length() - 2));
+		if (i == 0) {
+			i = 1;
+		}
+		int j = i + 1;
+		char random1 = word.charAt(i);
+		char random2 = word.charAt(j);
+		String Beginning = word.substring(0, i);                // creates a string before the randomly picked character
+		String End = word.substring(1 + j);                     // creates a string after the second picked character to the end of the word
+		scrambled = Beginning + random2 + random1 + End;        // concatenates the word with scrambled letters inside
 	}
 	return scrambled;
+
+
 }
 }
