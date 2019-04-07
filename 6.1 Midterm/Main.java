1bfdd6c77433c61234e126a3ed5dbf04c1b5ae15
laughtrey <laughtrey@gmail.com>
2019-04-07 15:04:10 -0700

    loops better and has methods now.

--- 6.1 Midterm/Main.java
+++ 6.1 Midterm/Main.java
@@ -1,77 +1,88 @@
 /*
-File: Main.java
-Description: Java midterm.
-Author: Raymond Laughrey
-Email: raymonl4963@student.vvc.edu
-Date of Creation: 4/4/2019, 7:12:16 PM
-*/
+   File: Main.java
+   Description: Java midterm.
+   Author: Raymond Laughrey
+   Email: raymonl4963@student.vvc.edu
+   Date of Creation: 4/4/2019, 7:12:16 PM
+ */
 import java.util.Scanner;
 import java.util.Vector;
 public class Main
 {
-   public static void main(String[] args)
-   {
-     Scanner input = new Scanner(System.in);
-     Vector<String> directory = new Vector<String>();
-     String name = " ";
-     String answer = " ";
-     boolean addmorenames = true;
-     boolean searchfornames = false;
-     while (addmorenames){
-       System.out.println("Hello, please enter the next name!");
-            name = input.next();
-            directory.add(name);
-        System.out.println("Would you like to enter another name? (y/n)");
-        answer = input.next();
-        while (!(answer.equals("y") || answer.equals("n"))) {
-      		System.out.println("Please enter 'y' or 'n'");
-      		answer = input.next();
-        }
-        if (answer.equals("y")) {
-          addmorenames = true;
-        }
-        else{
-          System.out.println("Would you like to search for the index of a name? (y/n)");
-          answer = input.next();
-          while (!(answer.equals("y") || answer.equals("n"))) {
-            System.out.println("Please enter 'y' or 'n'");
-            answer = input.next();
-          }
-          if (answer.equals("y")){
-          addmorenames = false;
-          searchfornames = true;
-        }
-        else
-        {
-          addmorenames = false;
-          searchfornames = false;
-        }
-        }
-        while (searchfornames){
-        System.out.println("Which name would you like to search for?");
-        String query = input.next();
-        for (int i = 0; i < directory.size();i++) {
-          if (directory.get(i).equals(query))
-          {
-            System.out.println("They are number: " + directory.indexOf(query)+1 +" in the list.");
-         }
-        }
-        System.out.println("Would you like to enter another name? (y/n)");
-        answer = input.next();
-        while (!(answer.equals("y") || answer.equals("n"))) {
-          System.out.println("Please enter 'y' or 'n'");
-          answer = input.next();
-        }
-        if (answer.equals("y")) {
-          addmorenames = true;
-          searchfornames = false;
-        }
-        else{
-          addmorenames = false;
-          searchfornames = false;
-        }
+public static void main(String[] args)
+{
+	Vector<String> directory = new Vector<String>();
+	String answer = " ";
+	boolean addmorenames = true;
+	boolean searchfornames = false;
+  Scanner input = new Scanner(System.in);
+	while (addmorenames) {
+		addmorenames(directory);
+		System.out.println("Would you like to enter another name? (y/n)");
+		answer = input.next();
+		while (!(answer.equals("y") || answer.equals("n"))) {
+			System.out.println("Please enter 'y' or 'n'");
+			answer = input.next();
+		}
+		if (answer.equals("y")) {
+			addmorenames = true;
+		}else  {
+			addmorenames = false;
+      searchfornames = true;
+		}
+		while (searchfornames) {
+			System.out.println("Would you like to search for the index of a name? (y/n)");
+			answer = input.next();
+			while (!(answer.equals("y") || answer.equals("n"))) {
+				System.out.println("Please enter 'y' or 'n'");
+				answer = input.next();
+			}
+			if (answer.equals("y")) {
+				addmorenames = false;
+				searchfornames = true;
+			}else  {
+				addmorenames = false;
+				searchfornames = false;
+			}
+			searchfornames(directory);
+			System.out.println("Would you like to enter another name or search? (e/s)");
+			answer = input.next();
+			while (!(answer.equals("e") || answer.equals("s"))) {
+				System.out.println("Please enter 'e' or 's'");
+				answer = input.next();
+			}
+			if (answer.equals("e")) {
+				addmorenames = true;
+				searchfornames = false;
+			}else if (answer.equals("s"))  {
+				addmorenames = false;
+				searchfornames = true;
+			}else {
+        addmorenames = false;
+        searchfornames = false;
       }
-     }
-     input.close();
+		}
+	}
+	input.close();
+}
+public static Vector<String> addmorenames(Vector<String> v)
+{
+  Scanner input = new Scanner(System.in);
+  String name = " ";
+	System.out.println("Hello, please enter the next name!");
+	name = input.next();
+	v.add(name);
+	return v;
+}
+public static void searchfornames(Vector<String> v)
+{
+  Scanner input = new Scanner(System.in);
+  System.out.println("Which name would you like to search for?");
+  String query = input.next();
+  for (int i = 0; i < v.size(); i++) {
+    if (v.get(i).equals(query)) {
+      System.out.println("They are number: " + v.indexOf(query) + 1 + " in the list.");
+    }
   }
+}
 }
